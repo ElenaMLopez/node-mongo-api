dea1dd89f31af296eae380262c01cb5ce9571ea2
elenamlopez <elena.mateos1@gmail.com>
2020-06-05 21:10:37 +0200

    Make the connection to database

--- server.js
+++ server.js
@@ -3,12 +3,9 @@ const CONFIG = require('./app/config/config');
 const App = require('./app/app');
 
 Database.connect();
-<<<<<<< HEAD
 
 App.listen(CONFIG.PORT, function(error) {
   if(error) return console.log('Error en el puerto de escucha: ', error)
   console.log('Conexión con BBDD Mongo exitosa');
   
 })
-=======
->>>>>>> cba3ed8a802552f661c4889b3686d8d3ad3e480e
