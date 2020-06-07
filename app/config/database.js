77f1a1bc2d32821cc8c667b58bc9239b48172d9b
elenamlopez <elena.mateos1@gmail.com>
2020-06-07 13:50:08 +0200

    Database config

--- app/config/database.js
+++ app/config/database.js
@@ -1,17 +1,24 @@
 /**
  * Here we have the Data Base connection
- */ 
-const mongoose = require('mongoose');
-const CONFIG = require('./config');
+ */
+
+const mongoose = require("mongoose");
+const CONFIG = require("./config");
 
 module.exports = {
   connection: null,
-  connect: function() {
-    if(this.connection) return this.connection;
-    return mongoose.connect(CONFIG.DB).then(connection => {
-      this.connection = connection;
-      console.log('Conexión con MongoDB exitosa! ')
-    }).catch(error => console.log('Error de conexión con MongoDB: ', error))
-  }
-}
-
+  connect: function () {
+    if (this.connection) return this.connection;
+    return mongoose
+      .connect(CONFIG.DB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+      })
+      .then((connection) => {
+        this.connection = connection;
+        console.log("Conexión con MongoDB exitosa! ");
+      })
+      .catch((error) => console.log("Error de conexión con MongoDB: ", error));
+  },
+};
