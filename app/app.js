d2ca6e94c95046eada32764a8b736e071fc74e5f
elenamlopez <elena.mateos1@gmail.com>
2020-06-07 14:23:34 +0200

    Add main route

--- app/app.js
+++ app/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const App = express()
+const App = express();
+
+const Product = require('./routes/product');
 
 App.use(bodyParser.json()); //Allow to send and recibe JSON format
-App.use(bodyParser.urlencoded({extended: false})); 
+App.use(bodyParser.urlencoded({ extended: false }));
 
-module.exports = App
+App.use('/product', Product); //This will be our root path "www.api.com/product"
+
+module.exports = App;
