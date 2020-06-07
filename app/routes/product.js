83d7943a67ba57edf08223e05f3593cdef43df62
elenamlopez <elena.mateos1@gmail.com>
2020-06-07 14:22:59 +0200

    Add product controller

--- app/routes/product.js
+++ app/routes/product.js
@@ -1,11 +1,12 @@
-const express = require('express');
-const ProductCtrl = require('../controllers/ProductController');
+const express = require("express");
+const ProductCtrl = require("../controllers/ProductController");
 
 const Router = express.Router();
 
-Router.get('/')               // api.com/product/
-      .get('/:key/:value')    // api.com/product/Hogar
-      .post('/:key/:value')   // api.com/product/
-      .put('/:key/:value')    // api.com/product/name/SamsungGalaxy
-      .delete('/:key/:value');// api.com/product/name/SamsunGalaxy
-module.exports = Router
+Router.get("/", ProductCtrl.index) // api.com/product/ Index: List all products
+  .get("/:key/:value", ProductCtrl.find, ProductCtrl.show) // api.com/product/Hogar Show: Show a product
+  .post("/:key/:value", ProductCtrl.find, ProductCtrl.create) // api.com/product/ Create: Create a product
+  .put("/:key/:value", ProductCtrl.find, ProductCtrl.update) // api.com/product/name/SamsungGalaxy Update: Edit and update a product
+  .delete("/:key/:value", ProductCtrl.find, ProductCtrl.remove); // api.com/product/name/SamsunGalaxy Delete: Remove a product
+
+module.exports = Router;
